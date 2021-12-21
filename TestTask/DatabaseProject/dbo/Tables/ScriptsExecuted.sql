14c3933a90d76d6edee7edc51c3b4e720697c7a8
ansar orkhan <ansar.orkhan@maxxle.com>
2021-12-21 20:13:04 +0400

    Added roles and mechanism for executng scripts only one time

---
+++ TestTask/DatabaseProject/dbo/Tables/ScriptsExecuted.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[ScriptsExecuted]
+(
+	[Id] INT IDENTITY (1, 1) NOT NULL,
+    [Name] NVARCHAR(50) NOT NULL, 
+    [ExecutedDate] DATETIME2 NULL
+    CONSTRAINT [PK_ExecutedScript] PRIMARY KEY CLUSTERED ([Id] ASC), 
+)
--- TestTask/DatabaseProject/dbo/Tables/User.sql
+++ TestTask/DatabaseProject/dbo/Tables/User.sql
@@ -1,9 +1,11 @@
 ﻿CREATE TABLE [dbo].[User] (
-    [Id]        INT           IDENTITY (1, 1) NOT NULL,
+    [Id] INT IDENTITY (1, 1) NOT NULL,
     [FirstName] NVARCHAR (50) NOT NULL,
     [LastName]  NVARCHAR (50) NOT NULL,
     [CreatedAt] DATETIME2 (7) NOT NULL,
-    [IsDeleted] BIT           CONSTRAINT [DF_User_IsDeleted] DEFAULT ((0)) NOT NULL,
-    CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([Id] ASC)
+    [RoleId] INT NOT NULL DEFAULT 1,
+    [IsDeleted] BIT CONSTRAINT [DF_User_IsDeleted] DEFAULT ((0)) NOT NULL,
+    CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([Id] ASC), 
+    CONSTRAINT [FK_User_Role] FOREIGN KEY ([RoleId]) REFERENCES [UserRole]([Id])
 );
 
